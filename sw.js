f9dfbe2a3f22fb5edf80528d3909ddc56ce11fe6
Constantin Zorile <czorile@gmail.com>
2021-03-25 22:52:47 +0200

    initial lighthouse audit

--- sw.js
+++ sw.js
@@ -2,16 +2,23 @@
  * add an event listener to the service worker to listen
  * to the Install Event (as part of Lifecycle events)
  * 
- * note: in browser Console, we can see "service worker has been installed".
+ * note: in browser console, we can see "service worker has been installed".
  * if we never change the content of the SW, we'll never see this 
  * message again (if we refresh the browser), because its content
  * is not changed.
  * if we change it, it then gets re-installed
  * 
  * also, SW remains "in waiting", until we close all of the tabs related 
- * to our application and reopen then.
+ * to our application and reopen then. (this can be changed from 
+ * browser console)
  */
-
 self.addEventListener('install', (evt) => {
   console.log("service worker has been installed");
+});
+
+/**
+ * listen for the activate event (and therefore activate SW)
+ */
+self.addEventListener("activate", (evt) => {
+  console.log("service worker has been activated");
 });
