05c206d1dc3b8d6743a3c47fd2c81a906b7c95b2
Constantin Zorile <czorile@gmail.com>
2021-03-27 21:59:20 +0200

    cache the assets, initial version

--- sw.js
+++ sw.js
@@ -1,31 +1,38 @@
-/**
- * add an event listener to the service worker to listen
- * to the Install Event (as part of Lifecycle events)
- * 
- * note: in browser console, we can see "service worker has been installed".
- * if we never change the content of the SW, we'll never see this 
- * message again (if we refresh the browser), because its content
- * is not changed.
- * if we change it, it then gets re-installed
- * 
- * also, SW remains "in waiting", until we close all of the tabs related 
- * to our application and reopen then. (this can be changed from 
- * browser console)
- */
+const PWACache = 'pwa-static-cache';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+/* listen to the install event (part of lifecycle events) */
 self.addEventListener('install', (evt) => {
-  console.log("service worker has been installed");
+  /**
+   * because we cache assets during install event, this event
+   * might end really fast and the browser might attempt to stop it
+   * before the actual process of caching finishes, therefore we 
+   * wrap this in waitUntil() method
+   */
+  evt.waitUntil(
+    caches.open(PWACache).then(cache => {
+      console.log("assets caching started...");
+       return cache.addAll(assets);
+    })
+  );
 });
 
-/**
- * listen for the activate event (and therefore activate SW)
- */
+/* listen for the activate event (and therefore activate SW) */
 self.addEventListener("activate", (evt) => {
   console.log("service worker has been activated");
 });
 
-/**
- * listen for the fetch event
- */
+/* listen for the fetch event */
 self.addEventListener("fetch", (evt) => {
   console.log("fetch event", evt);
 });
