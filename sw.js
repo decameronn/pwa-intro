7370037461769193feef4c5ccd6584fd3a5fcbda
Constantin Zorile <czorile@gmail.com>
2021-04-06 23:14:31 +0300

    dynamic cache allocation

--- sw.js
+++ sw.js
@@ -36,7 +36,13 @@ self.addEventListener("fetch", (evt) => {
   evt.respondWith(
     caches.match(evt.request)
       .then(cacheRes => {
-        return cacheRes || fetch(evt.request);
+        return cacheRes || fetch(evt.request)
+          .then(fetchRes => {
+            return caches.open(dynamicPWACache).then(cache => {
+              cache.put(evt.request.url, fetchRes.clone());
+              return fetchRes;
+            })
+          })
       })
   );
 });
