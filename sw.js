c08cac9968d7d322fe682bfe827eec09373951c6
Constantin Zorile <czorile@gmail.com>
2021-04-09 22:47:07 +0300

    add offline fallback page

--- sw.js
+++ sw.js
@@ -9,7 +9,8 @@ const assets = [
   '/css/styles.css',
   '/css/materialize.min.css',
   '/img/dish.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons'
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  '/pages/fallback.html'
 ];
 
 self.addEventListener('install', (evt) => {
@@ -25,7 +26,7 @@ self.addEventListener("activate", (evt) => {
   evt.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(keys
-        .filter(key => key !== PWACache) 
+        .filter(key => key !== PWACache && key !== dynamicPWACache) 
         .map(key => caches.delete(key))
       )
     })
@@ -34,15 +35,13 @@ self.addEventListener("activate", (evt) => {
 
 self.addEventListener("fetch", (evt) => {
   evt.respondWith(
-    caches.match(evt.request)
-      .then(cacheRes => {
-        return cacheRes || fetch(evt.request)
-          .then(fetchRes => {
+    caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
             return caches.open(dynamicPWACache).then(cache => {
               cache.put(evt.request.url, fetchRes.clone());
               return fetchRes;
             })
-          })
-      })
+          });
+      }).catch(() => caches.match("/pages/fallback.html"))
   );
 });
